d7eabc84cd1c2e052cc2f82695f040103d6b8ffd
Stephan Djurhuus <stephan.mdd@gmail.com>
2022-09-24 13:04:15 +0200

    add framer motion for animations

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+deposit * ( 100 - low / high * 100  ) ** iterations - deposit
+```
+
+- Migrate to 
+
+https://github.com/microsoft/onnxruntime/tree/main/js#onnxruntime-web
--- application/src/hooks/notification.tsx
+++ application/src/hooks/notification.tsx
@@ -1,43 +1,48 @@
 // dependencies
-import { createContext, Dispatch, FC, SetStateAction, useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
+import { createContext, Dispatch, FC, SetStateAction } from 'react';
+import { motion, Variant } from 'framer-motion';
 import styled from 'styled-components';
+import { v4 as uuid } from 'uuid';
 
 export type NotificationHook = {
-	info: (notification: Omit<NotificationProps, 'type'>) => void;
-	success: (notification: Omit<NotificationProps, 'type'>) => void;
-	warning: (notification: Omit<NotificationProps, 'type'>) => void;
-	error: (notification: Omit<NotificationProps, 'type'>) => void;
+	info: (notification: Omit<NotificationProps, 'type' | 'uuid'>) => void;
+	success: (notification: Omit<NotificationProps, 'type' | 'uuid'>) => void;
+	warning: (notification: Omit<NotificationProps, 'type' | 'uuid'>) => void;
+	error: (notification: Omit<NotificationProps, 'type' | 'uuid'>) => void;
 	clear: () => void;
 };
 
 export type NotificationProps = {
 	type: 'info' | 'success' | 'warning' | 'error';
+	onClick?: () => void;
 	title: string;
 	message?: string;
 	duration?: number;
+	uuid: string;
 };
 
 export const useNotification = (): NotificationHook => {
 	const setNotifications = useContext(NotificationContext);
 
-	function info(notification: Omit<NotificationProps, 'type'>): void {
+	function info(notification: Omit<NotificationProps, 'type' | 'uuid'>): void {
 		if (setNotifications === null) return;
-		setNotifications((notifications) => [...notifications, { ...notification, type: 'info' }]);
+		setNotifications((notifications) => [...notifications, { ...notification, type: 'info', uuid: uuid() }]);
 	}
 
-	function success(notification: Omit<NotificationProps, 'type'>): void {
+	function success(notification: Omit<NotificationProps, 'type' | 'uuid'>): void {
 		if (setNotifications === null) return;
-		setNotifications((notifications) => [...notifications, { ...notification, type: 'success' }]);
+		setNotifications((notifications) => [...notifications, { ...notification, type: 'success', uuid: uuid() }]);
 	}
 
-	function warning(notification: Omit<NotificationProps, 'type'>): void {
+	function warning(notification: Omit<NotificationProps, 'type' | 'uuid'>): void {
 		if (setNotifications === null) return;
-		setNotifications((notifications) => [...notifications, { ...notification, type: 'warning' }]);
+		setNotifications((notifications) => [...notifications, { ...notification, type: 'warning', uuid: uuid() }]);
 	}
 
-	function error(notification: Omit<NotificationProps, 'type'>): void {
+	function error(notification: Omit<NotificationProps, 'type' | 'uuid'>): void {
 		if (setNotifications === null) return;
-		setNotifications((notifications) => [...notifications, { ...notification, type: 'error' }]);
+		setNotifications((notifications) => [...notifications, { ...notification, type: 'error', uuid: uuid() }]);
 	}
 
 	function clear(): void {
@@ -48,35 +53,135 @@ export const useNotification = (): NotificationHook => {
 	return { info, success, warning, error, clear };
 };
 
+// stacking issue, unmounting error
 export const Notification: FC<NotificationProps> = (props) => {
+	type AnimationVariants = 'mounting' | 'mounted' | 'unmounting';
+
+	const mountedDuration = 0.5;
+	const unmountingDuration = 1;
+
 	const duration: number = props.duration ?? 4000;
-	const [expired, setExpired] = useState<boolean>(false);
+	const [animationVariant, setAnimationVariant] = useState<AnimationVariants>('mounting');
+	const setNotifications = useContext(NotificationContext);
 
-	useEffect(() => {
-		setTimeout(() => setExpired(true), duration);
-	}, [duration]);
+	function handleClick(): void {
+		if (setNotifications === null) return;
+		setAnimationVariant('unmounting');
+	}
 
-	if (expired) return null;
+	function handleAnimationEnd(variant: AnimationVariants) {
+		if (variant === 'mounting') {
+			setAnimationVariant('mounted');
+		}
+
+		if (variant === 'mounted') {
+			setTimeout(() => {
+				setAnimationVariant('unmounting');
+			}, duration);
+		}
+
+		if (variant === 'unmounting') {
+			if (setNotifications === null) return;
+			setNotifications((notifications) => notifications.filter(({ uuid }) => uuid !== props.uuid));
+		}
+	}
+
+	const variants: Record<AnimationVariants, Variant> = {
+		mounting: {
+			x: '110%',
+			scale: 0.5,
+			opacity: 0,
+		},
+
+		mounted: {
+			x: '0%',
+			scale: 1,
+			opacity: 1,
+			transition: { duration: mountedDuration, type: 'spring' },
+		},
+
+		unmounting: {
+			x: '110%',
+			scale: 0.5,
+			opacity: 0,
+			transition: { duration: unmountingDuration, type: 'spring' },
+		},
+	};
 
 	return (
-		<StyledNotification type={props.type}>
-			<h4>{props.title}</h4>
-			{props.message && <p>{props.message}</p>}
-			{/* progres loader */}
+		<StyledNotification
+			className={props.type}
+			onClick={props.onClick ?? handleClick}
+			variants={variants}
+			initial={{ x: '110%' }}
+			animate={animationVariant}
+			onAnimationComplete={handleAnimationEnd}
+		>
+			<div className="notification-content-container">
+				<h4>{props.title}</h4>
+				{props.message && <p>{props.message}</p>}
+			</div>
+
+			<svg>
+				<rect x={0} y={0} height="100%" width="100%" fill="transparent" />
+				<motion.rect
+					initial={{ width: '0%' }}
+					animate={{ width: '100%' }}
+					transition={{ delay: mountedDuration, duration: duration / 1000, ease: 'linear' }}
+					x={0}
+					y={0}
+					height="100%"
+					width="100%"
+					fill="red"
+				/>
+			</svg>
 		</StyledNotification>
 	);
 };
 
-type StyledNotificationProps = Pick<NotificationProps, 'type'>;
-
-// animations
-const StyledNotification = styled.div<StyledNotificationProps>`
+const StyledNotification = styled(motion.div)`
 	background-color: white;
-	padding: 1ch 2ch;
 	width: 350px;
 
 	border-radius: 1ch;
 	box-shadow: 0 1ch 1ch #0004;
+	overflow: hidden;
+
+	display: flex;
+	flex-direction: column;
+
+	.notification-content-container {
+		padding: 1ch 2ch;
+	}
+
+	svg {
+		width: 100%;
+		height: 0.6ch;
+	}
+
+	&.info {
+		h4 {
+			color: dodgerblue;
+		}
+	}
+
+	&.success {
+		h4 {
+			color: limegreen;
+		}
+	}
+
+	&.warning {
+		h4 {
+			color: orange;
+		}
+	}
+
+	&.error {
+		h4 {
+			color: tomato;
+		}
+	}
 `;
 
 export type NotificationContextValue = Dispatch<SetStateAction<NotificationProps[]>> | null;
@@ -104,7 +209,7 @@ const StyledNotifications = styled.div`
 	position: absolute;
 	top: 0;
 	right: 0;
-	margin: 1rem;
+	padding: 2ch;
 
 	display: flex;
 	flex-direction: column;
